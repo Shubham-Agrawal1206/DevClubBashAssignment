5c9321eac4756a4c894db7706d72eb5c239b44e8
shubham1206agra <tt1191044@iitd.ac.in>
2019-12-22 14:47:44 +0530

    Final v2

--- countFiles.sh
+++ countFiles.sh
@@ -5,11 +5,23 @@ then
 for FILE in $1/*.$2
 do 
     COUNT=`expr $COUNT + 1`
+    if [ ! -e $FILE ]
+    then
+    COUNT=0 
+    echo $COUNT
+    exit 1
+    fi
 done
 else
 for FILE in $1/*
 do 
     COUNT=`expr $COUNT + 1`
+    if [ ! -e $FILE ]
+    then
+    COUNT=0 
+    echo $COUNT
+    exit 1
+    fi
 done
 fi
 echo $COUNT
