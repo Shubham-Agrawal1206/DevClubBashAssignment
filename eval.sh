31bfde1ed01cfdbd876e746f53ec5220bcb98e51
shubham1206agra <tt1191044@iitd.ac.in>
2019-12-22 19:35:45 +0530

    final

--- eval.sh
+++ eval.sh
@@ -4,10 +4,20 @@ while IFS= read -r line
 do
     fe=`echo $line | tr -d '\r' | tr -d '\n'`
     op=${fe: -1}
+    STRLENGTH2=`echo -n $op | wc -m`
+    if [ $STRLENGTH2 != 1 ]
+    then
     STRLENGTH=`echo -n $fe | wc -m`
-    STRLENGTH=`expr $STRLENGTH - 2`
+    STRLENGTH=`expr $STRLENGTH - $STRLENGTH2`
+    STRLENGTH=`expr $STRLENGTH - 1`
+    num=${fe: 0:$STRLENGTH}
+    COUNTER=`expr $COUNTER \* $num`
+    continue
+    fi
+    STRLENGTH=`echo -n $fe | wc -m`
+    STRLENGTH=`expr $STRLENGTH - $STRLENGTH2`
+    STRLENGTH=`expr $STRLENGTH - 1`
     num=${fe: 0:$STRLENGTH}
     COUNTER=`expr $COUNTER $op $num`
-    echo $op
 done < "$1"
 echo $COUNTER
