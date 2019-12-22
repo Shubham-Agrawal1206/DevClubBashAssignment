0c63be574a9944cc340bae8050daef26769061ee
shubham1206agra <tt1191044@iitd.ac.in>
2019-12-22 23:13:53 +0530

    final

--- uname.sh
+++ uname.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-touch ip.txt
 strlen_com=`echo -n $2 | wc -m`
 while IFS= read -r line
+count=4
 do
     fe=`echo $line | tr -d '\r' | tr -d '\n'`
-    STRLENGTH=`echo -n $fe | wc -m`
-    num=${fe: 0:$STRLENGTH}
+    if [ ${fe:0:$strlen_com} = $2 ]
+    then
+    str=`echo $fe | tr ":" " "`
+    set -- $str
+    re=`expr $# - 2`
+    a=( $str )
+    string=""
+    while [ $count -lt $re ]
+    do
+    string="$string ${a[$count]}"
+    count=`expr $count + 1`
+    done
+    echo $string
+    exit 0
+    fi
 done < "$1"
