ba51126fae41496661be1e2db144b17c2ecee90a
shubham1206agra <tt1191044@iitd.ac.in>
2019-12-22 17:28:18 +0530

    mid

--- eval.sh
+++ eval.sh
@@ -2,5 +2,12 @@
 COUNTER=0
 while IFS= read -r line
 do
-    echo $line
+    fe=`echo $line | tr -d '\r' | tr -d '\n'`
+    op=${fe: -1}
+    STRLENGTH=`echo -n $fe | wc -m`
+    STRLENGTH=`expr $STRLENGTH - 2`
+    num=${fe: 0:$STRLENGTH}
+    COUNTER=`expr $COUNTER $op $num`
+    echo $op
 done < "$1"
+echo $COUNTER
